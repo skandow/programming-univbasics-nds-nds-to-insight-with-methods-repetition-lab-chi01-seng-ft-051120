4c59c62815602eb08ac47894c2c3b7afab3700ef
Steven Kandow <kando1sr@gmail.com>
2020-03-12 19:30:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,12 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  directors_names = []
+  row_index = 0 
+  while row_index < source.length do 
+    directors_names << source.row_index
+    row_index += 1
+  end
 end
 
 def total_gross(source)
