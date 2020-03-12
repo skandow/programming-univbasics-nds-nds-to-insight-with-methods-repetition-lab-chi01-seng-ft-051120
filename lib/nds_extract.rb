6aea5674e22145d11412f4e54202ac6ec2749fee
Steven Kandow <kando1sr@gmail.com>
2020-03-12 19:46:05 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,10 +25,26 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  directors_names = []
+  row_index = 0 
+  while row_index < source.length do 
+    directors_names << source[row_index][:name]
+    row_index += 1
+  end
+  directors_names
 end
 
 def total_gross(source)
+  total_everyone = 0 
+  director_name_gross = directors_totals(source)
+  director_name = list_of_directors(source)
+  big_index = 0 
+  while big_index < director_name.length do 
+    name_lookup = director_name[big_index]
+    total_everyone += director_name_gross[name_lookup]
+    big_index += 1
+  end 
+  total_everyone
   # Write this implementation
   #
   # Should use methods:
